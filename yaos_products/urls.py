0169b657299093d6ab7352f03b2551e3c1b0051d
Rangel Ivanov <rangelivanov88@gmail.com>
2012-06-11 22:27:40 +0300

    Added simple all_products view

--- yaos_products/urls.py
+++ yaos_products/urls.py
@@ -3,4 +3,5 @@ from views import *
 
 urlpatterns = patterns('',
         url(r'category/$', products_from_category),
+        url(r'^all', all_products),
 )
--- yaos_products/views.py
+++ yaos_products/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
+from yaos_products.models import Product
 
 def products_from_category(request):
     pass
+
+def all_products(request):
+    return render_to_response('products.html', {'products': Product.objects.all(), 'is_logged': request.user.is_authenticated(), }, context_instance=RequestContext(request))
