7c3abb8ad17271df213c711e61e251ac4c908e74
Rangel Ivanov <rangelivanov88@gmail.com>
2012-06-11 19:52:13 +0300

    Added app for home page

--- yaos/urls.py
+++ yaos/urls.py
@@ -8,6 +8,6 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', direct_to_template, {'template': 'base.html'}, name="index"),
+    url(r'^$', include('yaos_home.urls')),
     url(r'accounts/', include('yaos_auth.urls')),
 )
---
+++ yaos_home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+
+
+def home(request):
+    return render_to_response("base.html", { 'is_logged': request.user.is_authenticated(), }, context_instance=RequestContext(request))
+
