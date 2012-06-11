b73b48a92271bccddd625282112b45e622b700f6
Rangel Ivanov <rangelivanov88@gmail.com>
2012-06-11 18:47:50 +0300

    Added url rules for login and logout actions

--- yaos_auth/urls.py
+++ yaos_auth/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls.defaults import *
 from views import *
+from django.contrib.auth.views import login, logout
 
 urlpatterns = patterns('',
         url(r'register/$', register, name="register"),
+        url(r'login/$', login, name="login"),
+        url(r'logout/$', logout, name="logout"),
+
 )
