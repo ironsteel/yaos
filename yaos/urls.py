f16251c83daed5b527efa08017d3e25c759458fd
Rangel Ivanov <rangelivanov88@gmail.com>
2012-06-11 18:31:12 +0300

    Change directory of templates and static files.

--- yaos/urls.py
+++ yaos/urls.py
@@ -7,16 +7,7 @@ admin.autodiscover()
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'yaos.views.home', name='home'),
-    # url(r'^yaos/', include('yaos.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-   
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', direct_to_template, {'template': 'base.html'}, name="index"),
     url(r'accounts/', include('yaos_auth.urls')),
-
 )
