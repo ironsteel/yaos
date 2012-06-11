872638c90173f8c54cb0991d9198b15931e76a74
Rangel Ivanov <rangelivanov88@gmail.com>
2012-06-11 19:52:57 +0300

    Added simple logout view function
    Include is logged var in base template

--- yaos_auth/urls.py
+++ yaos_auth/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth.views import login, logout
 urlpatterns = patterns('',
         url(r'register/$', register, name="register"),
         url(r'login/$', login, name="login"),
-        url(r'logout/$', logout, name="logout"),
+        url(r'logout/$', logout_view),
 
 )
--- yaos_auth/views.py
+++ yaos_auth/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from forms import RegisterForm
+from django.contrib.auth import logout
 
 def register(request):
     if request.method == 'POST':
@@ -11,4 +12,8 @@ def register(request):
             return HttpResponseRedirect("/");
     else:
         form = RegisterForm()
-    return render_to_response("register.html", {'form': form, }, context_instance=RequestContext(request))
+    return render_to_response("register.html", {'form': form, 'logged_in': request.user.is_authenticated(), }, context_instance=RequestContext(request))
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect("/")
