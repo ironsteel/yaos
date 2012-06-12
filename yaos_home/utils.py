bfe454b59595c62cd74d6aa4588eca62c5e7f67b
Rangel Ivanov <rangelivanov88@gmail.com>
2012-06-12 11:09:33 +0300

    Calculate total price and show number of products in cart

--- yaos_home/utils.py
+++ yaos_home/utils.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
-from yaos_products.models import Category
+from yaos_products.models import Category, ShoppingCart, Product
 
 
 def add_common_vars(request, d):
     d['is_logged'] = request.user.is_authenticated()
     d['categories'] = Category.objects.all()
     d['current_user'] = request.user
+    products_in_cart = ShoppingCart.objects.filter(user__id=request.user.id)
+    d['products_in_cart'] = len(products_in_cart)
+    total = 0
+    for cart_item in products_in_cart:
+        total += cart_item.product.price
+    d['total'] = total
     return d
