4b14d29d3fcb1768dc8f4eed2351a06ca53216d5
Rangel Ivanov <rangelivanov88@gmail.com>
2012-06-12 08:21:26 +0300

    Added new model for picture Added product price Show price and picture in template

--- yaos_products/admin.py
+++ yaos_products/admin.py
@@ -1,5 +1,6 @@
-from yaos_products.models import Product, Category
+from yaos_products.models import Product, Category, Picture
 from django.contrib import admin
 
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Picture)
--- yaos_products/models.py
+++ yaos_products/models.py
@@ -1,6 +1,22 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from PIL import Image
+import os
+import tempfile
+from django.core.files.base import File
+
+class Picture(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images')
+    def save(self, **kwargs):
+        super(Picture, self).save(**kwargs)
+        path = self.image.path
+        i = Image.open(path)
+        i.thumbnail((94 ,94), Image.BILINEAR)
+        i.save(path)
+    def __unicode__(self):
+        return self.name
 
 
 class Category(models.Model):
@@ -14,10 +30,10 @@ class Product(models.Model):
     brand = models.CharField(max_length=50)
     description = models.TextField()
     availability = models.IntegerField()
-    image_file = models.ImageField(upload_to="images")
+    image_file = models.ForeignKey(Picture)
+    price = models.FloatField()
     
     def __unicode__(self):
         return self.name
-
 
 
